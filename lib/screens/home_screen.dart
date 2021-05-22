92297c873c9e96ad6fa5d79c6ccbf75aa487d1af
samad karimi <samad.nkarimi@gmail.com>
2021-05-22 21:09:50 +0700

    some widgets added & body was repaired

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mbi_app/screens/drawer_screen.dart';
+import '../widgets/custom_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../widgets/radial_gauge.dart';
 
 class MBIHome extends StatefulWidget {
   MBIHome({Key key}) : super(key: key);
@@ -33,80 +36,78 @@ class _MBIHomeState extends State<MBIHome> {
     return Container(
       child: Scaffold(
         drawer: MbiDrawer(),
-        appBar: PreferredSize(
-          preferredSize: Size(double.infinity, 100),
-          child: Container(
-            color: Colors.white,
-            child: Stack(
-              children: [
-                Positioned(
-                  top: -160,
-                  right: -60,
-                  child: Container(
-                    constraints: BoxConstraints(
-                        minWidth: MediaQuery.of(context).size.width / 2.0),
-                    child: Image.asset(
-                      "assets/images/ellipse.png",
-                      scale: .85,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 40,
-                  right: 50,
-                  child: Text(
-                    "MBICalc",
-                    style: TextStyle(fontSize: 25, fontFamily: "ANasr"),
-                  ),
-                ),
-                Positioned(
-                  top: 10,
-                  left: 0,
-                  child: Builder(
-                    builder: (context) => Container(
-                      padding: const EdgeInsets.all(10.0),
-                      margin: const EdgeInsets.all(10.0),
-                      alignment: Alignment.topLeft,
-                      child: IconButton(
-                        iconSize: 50,
-                        icon: Image.asset("assets/icons/menu.png"),
-                        onPressed: () => Scaffold.of(context).openDrawer(),
+        body: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Positioned(
+                    top: 10,
+                    left: 0,
+                    child: Builder(
+                      builder: (context) => Container(
+                        padding: const EdgeInsets.all(10.0),
+                        margin: const EdgeInsets.all(10.0),
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          iconSize: 50,
+                          icon: Image.asset("assets/icons/menu.png"),
+                          onPressed: () => Scaffold.of(context).openDrawer(),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        body: Column(
-          children: [
-            Card(
-              child: Text("Salam"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Card(
-                elevation: 2,
-                // shape,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Column(
+                  Stack(
+                    alignment: Alignment.center,
                     children: [
-                      _scoreRow(" very severely underweight", "=< 15.9"),
-                      _scoreRow("severely underweight", "16.0 - 16.9"),
-                      _scoreRow("underweight", "17.0 - 18.4"),
-                      _scoreRow("normal", "18.5 - 24.9"),
-                      _scoreRow("overweight", "25.0 - 29.9"),
-                      _scoreRow("obese class|", "30.0 - 34.9"),
-                      _scoreRow("obese class||", "35.0 - 39.9"),
-                      _scoreRow("obese class|||", ">=40.0"),
+                      Container(
+                        // constraints: BoxConstraints(
+                        //     minWidth: MediaQuery.of(context).size.width / 2.0),
+                        child: SvgPicture.asset("assets/images/top_shape.svg"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Text(
+                          "MBICalc",
+                          style: TextStyle(fontSize: 25, fontFamily: "ANasr"),
+                        ),
+                      ),
                     ],
+                  ),
+                ],
+              ),
+              RadialGauge(),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    // side: BorderSide(width: 5, color: Colors.green),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      children: [
+                        SliderWidget(),
+
+                        // _scoreRow(" very severely underweight", "=< 15.9"),
+                        // _scoreRow("severely underweight", "16.0 - 16.9"),
+                        // _scoreRow("underweight", "17.0 - 18.4"),
+                        // _scoreRow("normal", "18.5 - 24.9"),
+                        // _scoreRow("overweight", "25.0 - 29.9"),
+                        // _scoreRow("obese class|", "30.0 - 34.9"),
+                        // _scoreRow("obese class||", "35.0 - 39.9"),
+                        // _scoreRow("obese class|||", ">=40.0"),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/radial_gauge.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class RadialGauge extends StatelessWidget {
+  const RadialGauge({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SfRadialGauge(
+      title: GaugeTitle(
+          text: 'Speedometer',
+          textStyle:
+              const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+      axes: <RadialAxis>[
+        RadialAxis(
+          tickOffset: 40,
+          minimum: 0,
+          maximum: 120,
+          startAngle: 140,
+          endAngle: 40,
+          ranges: <GaugeRange>[
+            GaugeRange(
+                rangeOffset: 0,
+                startValue: 0,
+                endValue: 40,
+                color: Colors.green,
+                startWidth: 60,
+                endWidth: 60),
+            GaugeRange(
+                startValue: 40,
+                endValue: 80,
+                color: Colors.orange,
+                startWidth: 60,
+                endWidth: 60),
+            GaugeRange(
+                startValue: 80,
+                endValue: 120,
+                color: Colors.red,
+                startWidth: 60,
+                endWidth: 60)
+          ],
+          pointers: <GaugePointer>[
+            NeedlePointer(
+              value: 30,
+              tailStyle: TailStyle(width: 8, length: 0.08),
+              lengthUnit: GaugeSizeUnit.factor,
+              needleLength: 0.4,
+            )
+          ],
+          annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+                widget: Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blueAccent, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    child: const Text('90.0',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold))),
+                angle: 90,
+                positionFactor: 0.3)
+          ],
+        )
+      ],
+    );
+  }
+}
